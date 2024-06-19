7ef5c839ebd3ff7541a794cd9b4ad846da3596f3
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-06-19 10:31:04 +0900

    Spring Bootを活用したRESTful TODOリストAPIの構築（オリジナル課題）下記機能の追加 ・Lombokの利用

--- src/main/java/com/original/RestAPI_TodoList1/model/TodoItem.java
+++ src/main/java/com/original/RestAPI_TodoList1/model/TodoItem.java
@@ -3,6 +3,11 @@ package com.original.RestAPI_TodoList1.model;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TodoItem {
 	
 	private String id;
@@ -24,54 +29,5 @@ public class TodoItem {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		this.create_datetime = sdf.format(cl.getTime());
 		this.update_datetime = sdf.format(cl.getTime());
-	}
-
-	public String getCreate_datetime() {
-		return create_datetime;
-	}
-
-	public void setCreate_datetime(String create_datetime) {
-		this.create_datetime = create_datetime;
-	}
-	
-
-	public String getUpdate_datetime() {
-		return update_datetime;
-	}
-
-	public void setUpdate_datetime(String update_datetime) {
-		this.update_datetime = update_datetime;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public String getDetails() {
-		return details;
-	}
-
-	public void setDetails(String details) {
-		this.details = details;
 	}
 }
